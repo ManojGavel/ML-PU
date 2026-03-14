d227f068231a558b9e47f08425337a58ed4d94a4
Manoj Gavel <121299626+ManojGavel@users.noreply.github.com>
2026-03-14 16:22:55 +0530

    changes done

--- webapp.py
+++ webapp.py
@@ -7,4 +7,4 @@ sw=st.slidet("sw",2.0,10.0)
 PL=st.slidet("PL",2.0,10.0)
 PW=st.slidet("PW",2.0,10.0)
 if st.button("Predict"):
- st. success model. predict([[sl, sw, pl, pw]]))
+ st. success model. predict([[sl, sw, pl, pw]])
